af19703b0d6746b114ee213c51958fd0c896d2b0
Felix Klaedtke <felix.klaedtke@gmail.com>
2026-04-05 09:30:36 +0200

    doc.go comment

--- go/doc.go
+++ go/doc.go
@@ -7,3 +7,10 @@ package sliding
 // algorithm here.  This package is self contained, i.e., only packages from
 // Go's standard library are used.  The more general sliding package provides
 // also other aggregation functions and relies on a few third-party packages.
+
+// We use some Go specific features in the algorithm's implementation.  Namely,
+// we use channels to receive stream elements and to send the aggregated values
+// of the sliding window.  This allows us to deal with potentially infinite
+// streams.  We also deviated from the algorithm's description in the paper for
+// obtaining a more efficient implementation.  But the core of the algorithm is
+// the same.
