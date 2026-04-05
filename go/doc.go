cd538f4e7fcfcd736c860cc92aa08065b765968a
Felix Klaedtke <felix.klaedtke@gmail.com>
2026-04-05 09:20:17 +0200

    doc.go comment

--- go/doc.go
+++ go/doc.go
@@ -1,3 +1,9 @@
 package sliding
 
-// Go implementation of the sliding window algorithm.
+// Go implementation of the paper's sliding window algorithm.
+
+// This package provides a subset of the package github.com/flxch/sliding.
+// Here, we only focus on the implementation of the paper's algorithm.  It is
+// self contained, i.e., only packages from Go's standard library are used.  The
+// more general sliding package contains provides also other aggregation
+// functions and relies on a few third-party packages.
